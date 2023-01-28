57ddfa11f5504759a5c7c857f7f372a809506820
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-01-28 20:05:28 +0100

    remove contests of titleList

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ function titleClickHandler(event){
   console.log(articleSelector);
   
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle = document.querySelector('articleSelector')
+  const targetArticle = document.querySelector('articleSelector') 
   console.log(targetArticle)
   /* [IN PROGRESS] add class 'active' to the correct article */
   targetArticle.classList.add('active');
@@ -40,3 +40,27 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(customSelector = '') {
+  console.log()
+/* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector)
+  titleList.innerHTML = '';
+/* for each article */
+
+/* get the article id */
+
+/* find the title element */
+
+/* get the title from the title element */
+
+/* create HTML of the link */
+
+/* insert link into titleList */
+
+  }
+  generateTitleLinks();
