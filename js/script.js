d74a2e1c9b9545112e173f6a79356a163bed6dec
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-02 13:03:02 +0100

    Add szablon skryptu

--- js/script.js
+++ js/script.js
@@ -73,3 +73,31 @@ console.log(links)
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+function generateTags(){
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
