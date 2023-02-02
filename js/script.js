39aae2f4dc67299cfbd5e1338a6ac8118c1d0cbc
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-02 15:15:45 +0100

    find all acive tags

--- js/script.js
+++ js/script.js
@@ -119,11 +119,12 @@ function tagClickHandler(event){
   /* [DONE] make a new constant "href" and read the attribute "href" of the clicked element */
   const href = clickedElement.getAttribute('href')
   console.log(href)
-  /* [IN PROGRESS] make a new constant "tag" and extract tag from the "href" constant */
+  /* [DONE] make a new constant "tag" and extract tag from the "href" constant */
   const tag = href.replace('#tag-', '');
   console.log(tag)
-  /* find all tag links with class active */
-
+  /* [IN PROGRESS] find all tag links with class active */
+  const tagLinks = document.querySelectorAll('a.actve[href^="tag-"]')
+  console.log(tagLinks)
   /* START LOOP: for each active tag link */
 
     /* remove class active */
