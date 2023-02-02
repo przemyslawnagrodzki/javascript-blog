6bddd7fb1519a32e354e5394d727dafdd1d20926
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-02 14:31:57 +0100

    generate html of the link

--- js/script.js
+++ js/script.js
@@ -93,19 +93,19 @@ function generateTags(){
     /* [DONE] split tags into array */
     const articleTagsArray = articleTags.split(' ');
     console.log(articleTagsArray)
-    /* [IN PROGRESS] START LOOP: for each tag */
+    /* [DONE] START LOOP: for each tag */
     for(let tag of articleTagsArray) {
       console.log(tag)
-      /* generate HTML of the link */
-
-      /* add generated code to html variable */
-
+      /* [IN PROGRESS] generate HTML of the link */
+      const tagLinkHTML = '<li><a href="#tag'- + articleTagsArray + '"><span>' + articleTagsArray + '</span></a></li>';
+      console.log(linkHTML)
+      /* [IN PROGRESS] add generated code to html variable */
+      html = html + tagLinkHTML
     /* END LOOP: for each tag */
-
+    }
     /* insert HTML of all the links into the tags wrapper */
 
   /* END LOOP: for every article: */
-}
 }
 }
 generateTags();
