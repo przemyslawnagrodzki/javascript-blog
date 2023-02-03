4d61e93ad44c1a6f9720c91e785d075c1d5bb4a2
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-03 14:25:19 +0100

    generate links to tag list

--- js/script.js
+++ js/script.js
@@ -121,9 +121,16 @@ function generateTags(){
 /* [NEW] find list of tags in right column */
 const tagList = document.querySelector(optTagsListSelector);
 
-/* [NEW] add html from allTags to tagList */
-// tagList.innerHTML = allTags.join(' '); //
-console.log(allTags)
+/* [NEW] create variable for all links HTML code */
+let allTagsHTML = '';
+/* [NEW] START LOOP: for each tag in allTags */
+for(let tag in allTags) {
+/* [NEW] generate code of a link and add in allTagsHTML: */
+allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+/* [NEW] END LOOP for each tag in allTags */
+}
+/* [NEW] add html from allTagsHTML to tagList */
+tagList.innerHTML = allTagsHTML;
 }
 generateTags();
 
